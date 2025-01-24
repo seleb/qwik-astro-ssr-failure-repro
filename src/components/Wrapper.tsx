d0fb0c343f3820c3ab34c987ac81b88ad931c5e2
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2025-01-24 11:49:31 -0500

    add `InlineComponentPropsSpread` to index and wrapper cases

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,9 +1,11 @@
+import { InlineComponentPropsSpread } from './InlineComponentPropsSpread';
 import { InlineComponentSimple } from './InlineComponentSimple';
 
 export const Wrapper = () => {
   return (
     <div>
       <InlineComponentSimple />
+      <InlineComponentPropsSpread />
     </div>
   );
 };
